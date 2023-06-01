03181ca69c3da2d03c24efe2d3f9a932225b9d26
amnioc <nicdhillon19@gmail.com>
2023-06-01 14:21:15 +0100

    add picture

--- dice_roller/src/components/Description.js
+++ dice_roller/src/components/Description.js
@@ -1,7 +1,12 @@
 const Description = () => {
   return (
     <section id="description" aria-label="app description">
-      <h3>Welcome!</h3>
+      <h2>Welcome!</h2>
+      <img
+        src="https://images.unsplash.com/photo-1625173925005-0043774fe954?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
+        alt="green dice on white background"
+        id="description-image"
+      />
       <br />
       1. Please <span className="focus-word"> enter your number</span> of dice
       being thrown (if empty, we'll assume 1). <br /> 2. Then, choose how many
