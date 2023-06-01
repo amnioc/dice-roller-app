74040573af9517ffce55159340fec6cd45dea37d
amnioc <nicdhillon19@gmail.com>
2023-06-01 15:11:06 +0100

    screenshot, screen changes

--- dice_roller/src/components/Description.js
+++ dice_roller/src/components/Description.js
@@ -1,7 +1,7 @@
 const Description = () => {
   return (
     <section id="description" aria-label="app description">
-      <h2>Welcome!</h2>
+      <h2>Welcome, Player!</h2>
       <img
         src="https://images.unsplash.com/photo-1625173925005-0043774fe954?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
         alt="green dice on white background"
